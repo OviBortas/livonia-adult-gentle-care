ba3bc77f5348143632bfbc5d89ae48fb00383def
Ovi Bortas <ovib313@gmail.com>
2016-10-08 14:26:50 -0400

    Update NewsVC
    Add buttons to navigation bar
    Add horizontal bar under selected button
    Add animations for horizontal bar

--- LivoniaAdultGentleCare/LivoniaAdultGentleCare/NewsVC.swift
+++ LivoniaAdultGentleCare/LivoniaAdultGentleCare/NewsVC.swift
@@ -10,14 +10,65 @@ import UIKit
 
 class NewsVC: UIViewController {
     
-    let gradient: CAGradientLayer = CAGradientLayer()
+    //let gradient: CAGradientLayer = CAGradientLayer()
 
+    @IBOutlet weak var horizontalBar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Determin in which direction the translation should be
+        let isOnLefHand = horizontalBar.frame.origin.x < view.bounds.size.width / 2
+        var translationX = horizontalBar.bounds.width * 2
+        translationX = isOnLefHand ? translationX.negated() : translationX
+        
+        // Create and animate the translation
+        horizontalBar.transform = CGAffineTransform(translationX: translationX, y: 0)
+        
+        UIView.animate(withDuration: 0.7, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [.beginFromCurrentState, .curveEaseOut], animations: {
+            self.horizontalBar.transform = CGAffineTransform.identity
+            }, completion: nil)
+    }
+    
+    @IBAction func ofIntrestButtonTappred(_ sender: UIButton) {
+        animateHorizontalBar(to: sender)
+    }
+    
+    @IBAction func updatesTapped(_ sender: UIButton) {
+        animateHorizontalBar(to: sender)
+    }
+    
+    fileprivate func animateHorizontalBar(to destination: UIButton) {
+        let identifier = "AlignCenterX"
+        // Locate the constraint and gaurd for nil value
+        let constraintX = horizontalBar.constraintsAffectingLayout(for: .horizontal).first{ $0.identifier == identifier }
+        guard let constraint = constraintX else {
+            print("Found no constraint at", #function, #line)
+            return
+        }
+        
+        // Remove old constraint and add new constraint
+        constraint.isActive = false
+        horizontalBar.removeConstraint(constraint)
+        
+        let newConstraint = horizontalBar.centerXAnchor.constraint(equalTo: destination.centerXAnchor, constant: 0)
+        newConstraint.identifier = identifier
+        newConstraint.isActive = true
+        
+        
+        // Animate the constraint change
+        UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, options: [.beginFromCurrentState, .curveEaseOut], animations: {
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+        
+//        UIView.animate(withDuration: 1.0) { 
+//            self.view.layoutIfNeeded()
+//        }
+    }
 
 
     /*
